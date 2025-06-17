fca376acbae57180928423a127e5a8b50e60e887
Lyzil <gu.verissimomonteiro@gmail.com>
2025-06-16 22:43:23 -0300

    Update and rename filasAtendimento.cpp to filasAtendimentoV2.cpp

--- filasAtendimentoV2.cpp
+++ filasAtendimentoV2.cpp
@@ -1,96 +1,28 @@
 /******************************************************************************
 
-Implementar um sistema simples de atendimento com a oferta de 3 opções através de um seletor:
+Implementar (através de ponteiros) a versão 2.0 do sistema de atendimento já realizado, agora com a oferta de 5 opções através de um seletor:
 
 0. Sair
 1. Gerar senha
-2. Realizar atendimento
-
-
+2. Abrir guichê
+3. Realizar atendimento
+4. Listar senhas atendidas
 A opção 1 deverá incrementar o controle de senhas, gerando e enfileirando a senha gerada na fila "senhasGeradas"
 
-A opção 2 deverá consumir a senha "da vez", exibindo-a na tela e enfileirando-a na fila "senhasAtendidas".
+A opção 2 deverá adicionar, em uma estrutura do tipo Lista, um objeto (struct) contendo o id do guichê e uma fila (senhasAtendidas) para o armazenamento das senhas atendidas por ele
 
-Sempre que o seletor for exibido, informar a quantidade de senhas ainda aguardando atendimento.
+A opção 3 deverá solicitar a digitação do id do guichê que está chamando a senha "da vez", enfileirando-a na fila "senhasAtendidas" do respectivo guichê
+
+A opção 4 deverá solicitar a digitação do id do guichê e, na sequência, listar todas as senhas que foram atendidas por ele
+
+Sempre que o seletor for exibido, informar a quantidade de senhas ainda aguardando atendimento e o número de guichês abertos para atendimento.
 
 O programa só poderá ser encerrado quando a fila "senhasGeradas" estiver vazia. Ao encerrar, informar a quantidade de senhas atendidas.
-
-Obs: Desenvolver essa solução implementando as filas em forma ponteiro e de vetor.
 
 *******************************************************************************/
 
 #include <iostream>
 using namespace std;
-
-#define MAX 6 // add um a mais para poder funcionar corretamente
-
-// fila em formato de vetor
-
-struct FilaVetor
-{
-	int nos[MAX];
-	int ini;
-	int fim;
-};
-
-FilaVetor* initVetor()
-{
-	FilaVetor *f = new FilaVetor;
-	f->ini = 0;
-	f->fim = 0;
-	return f;
-}
-
-int isEmptyVetor(FilaVetor *f)
-{
-	return (f->ini == f->fim);
-}
-
-int incrementa(int i)
-{
-    return (i + 1) % MAX;
-}
-
-int countVetor(FilaVetor *f)
-{
-	int qtde = 0;
-	int i = f->ini;
-	while (i != f->fim)
-	{
-		qtde++;
-		i = incrementa(i);
-	}
-	return qtde;
-}
-int enqueueVetor(FilaVetor *f, int v)
-{
-	int podeEnfileirar = (incrementa(f->fim) != f->ini);
-	if (podeEnfileirar)
-	{
-		f->nos[f->fim] = v;
-		f->fim = incrementa(f->fim);
-	}
-	return (podeEnfileirar);
-}
-int dequeueVetor(FilaVetor *f)
-{
-	int ret;
-	if (isEmptyVetor(f))
-	{
-		ret = -1;
-	}
-	else
-	{
-		ret = f->nos[f->ini];
-		f->ini = incrementa(f->ini);
-	}
-	return ret;
-}
-
-void freeFilaVetor(FilaVetor *f)
-{
-	free(f);
-}
 
 //fila em formato de ponteiro.
 
@@ -184,14 +116,14 @@ void freeFilaPonteiro(FilaPonteiro *f)
 
 int main()
 {
-    FilaVetor* senhasGeradas = initVetor();
+    	FilaPonteiro* senhasGeradas = initPonteiro();
 	FilaPonteiro* senhasAtendidas = initPonteiro();
 	
 	int opcao = -1;
 	int contador = 1;
 
 	do {
-        cout << "Senhas aguardando atendimento: " << countVetor(senhasGeradas) << endl;
+        cout << "Senhas aguardando atendimento: " << countPonteiro(senhasGeradas) << endl;
         cout << "Menu de opções" << endl
              << "0. Sair" << endl
              << "1. Gerar senha" << endl
@@ -201,15 +133,15 @@ int main()
 
         switch (opcao) {
             case 0:
-                if (!isEmptyVetor(senhasGeradas)) {
-                    cout << "Ainda tem " << countVetor(senhasGeradas) << " senhas em espera. Atenda todas antes de sair." << endl;
+                if (!isEmptyPonteiro(senhasGeradas)) {
+                    cout << "Ainda tem " << countPonteiro(senhasGeradas) << " senhas em espera. Atenda todas antes de sair." << endl;
                     opcao = -1; // para manter o loop aberto
                 } else {
                     cout << "Senhas atendidas: " << countPonteiro(senhasAtendidas) << endl;
                 }       
                 break;
             case 1:
-                if (enqueueVetor(senhasGeradas, contador)) {
+                if (enqueuePonteiro(senhasGeradas, contador)) {
                     cout << "Senha gerada: " << contador << endl;
                     contador++;
                 } else {
@@ -217,7 +149,7 @@ int main()
                 }
                 break;
             case 2:{
-                int senha = dequeueVetor(senhasGeradas);
+                int senha = dequeuePonteiro(senhasGeradas);
                 if (senha == -1) {
                     cout << "Nenhuma senha para atender." << endl;
                 } else {
@@ -232,7 +164,7 @@ int main()
         }
     } while(opcao != 0);
     
-    freeFilaVetor(senhasGeradas);
+    freeFilaPonteiro(senhasGeradas);
     freeFilaPonteiro(senhasAtendidas);
 
     return 0;
