6e15efe1657278aa9fd5a38a9ef7b1b1d1228bee
Lyzil <gu.verissimomonteiro@gmail.com>
2025-06-17 15:32:04 -0300

    Update filasAtendimentoV2.cpp

--- filasAtendimentoV2.cpp
+++ filasAtendimentoV2.cpp
@@ -9,7 +9,8 @@ Implementar (através de ponteiros) a versão 2.0 do sistema de atendimento já 
 4. Listar senhas atendidas
 A opção 1 deverá incrementar o controle de senhas, gerando e enfileirando a senha gerada na fila "senhasGeradas"
 
-A opção 2 deverá adicionar, em uma estrutura do tipo Lista, um objeto (struct) contendo o id do guichê e uma fila (senhasAtendidas) para o armazenamento das senhas atendidas por ele
+A opção 2 deverá adicionar, em uma estrutura do tipo Lista, um objeto (struct) contendo o id do guichê e uma fila (senhasAtendidas) para o armazenamento das 
+senhas atendidas por ele
 
 A opção 3 deverá solicitar a digitação do id do guichê que está chamando a senha "da vez", enfileirando-a na fila "senhasAtendidas" do respectivo guichê
 
@@ -24,12 +25,21 @@ O programa só poderá ser encerrado quando a fila "senhasGeradas" estiver vazia
 #include <iostream>
 using namespace std;
 
-//fila em formato de ponteiro.
-
 struct No
 {
 	int dado;
 	No *prox;
+};
+
+struct NoGuiche {
+    guiches dado;
+    NoGuiche* prox;
+};
+
+struct guiches
+{
+    int IDguiche;
+    FilaPonteiro* SenhasAtendidasGuiche;
 };
 
 struct FilaPonteiro
@@ -117,50 +127,81 @@ void freeFilaPonteiro(FilaPonteiro *f)
 int main()
 {
     	FilaPonteiro* senhasGeradas = initPonteiro();
-	FilaPonteiro* senhasAtendidas = initPonteiro();
+	    NoGuiche* listaGuiches = NULL;
 	
-	int opcao = -1;
-	int contador = 1;
+	int opcao = -1, contador = 1, IDescolhido = -1, i = 0;
 
 	do {
         cout << "Senhas aguardando atendimento: " << countPonteiro(senhasGeradas) << endl;
         cout << "Menu de opções" << endl
              << "0. Sair" << endl
              << "1. Gerar senha" << endl
-             << "2. Realizar atendimento" << endl
+             << "2. Abrir guichê" << endl
+             << "3. Realizar atendimento" << endl
+             << "4. Listar senhas atendidas" << endl
              << "Escolha a opção que deseja: ";
 	    cin >> opcao;
 
-        switch (opcao) {
-            case 0:
-                if (!isEmptyPonteiro(senhasGeradas)) {
-                    cout << "Ainda tem " << countPonteiro(senhasGeradas) << " senhas em espera. Atenda todas antes de sair." << endl;
-                    opcao = -1; // para manter o loop aberto
-                } else {
-                    cout << "Senhas atendidas: " << countPonteiro(senhasAtendidas) << endl;
-                }       
-                break;
-            case 1:
-                if (enqueuePonteiro(senhasGeradas, contador)) {
-                    cout << "Senha gerada: " << contador << endl;
-                    contador++;
-                } else {
-                    cout << "Fila de senhas geradas cheia! Aguarde atendimento." << endl;
-                }
-                break;
-            case 2:{
-                int senha = dequeuePonteiro(senhasGeradas);
-                if (senha == -1) {
-                    cout << "Nenhuma senha para atender." << endl;
-                } else {
-                    enqueuePonteiro(senhasAtendidas, senha);
-                    cout << "Senha atendida: "<< senha << endl;
-                }
-                break;
+    switch (opcao) {
+        case 0:
+            if (!isEmptyPonteiro(senhasGeradas)) {
+                cout << "Ainda tem " << countPonteiro(senhasGeradas) << " senhas em espera. Atenda todas antes de sair." << endl;
+                opcao = -1; // para manter o loop aberto
+            } else {
+                cout << "Senhas atendidas: " << countPonteiro(senhasAtendidas) << endl;
+            }       
+            break;
+        case 1:
+            enqueuePonteiro(senhasGeradas, contador);
+            cout << "Senha gerada: " << contador << endl;
+            contador++;
+            break;
+        case 2:{
+            guiches novoGuiche;
+            novoGuiche.IDguiche = (i+1);
+            novoGuiche.SenhasAtendidasGuiche = initPonteiro();
+            i++;
+            
+            NoGuiche* novoNO = new NoGuiche;
+            novoNO->dado = novoGuiche;
+            novoNO->prox = listaGuiches;
+            listaGuiches = novoNO;
+            break;
+        }
+        case 3:
+        {
+            NoGuiche* aux = listaGuiches;
+            bool IDencontrado = false;
+            cout <<"Digite o ID do guichê: ";
+            cin >> IDescolhido;
+            while (aux != NULL)
+            {
+        		if(IDescolhido == aux->dado.IDguiche)
+        		{
+        		    int senha = dequeuePonteiro(senhasGeradas);
+                    if (senha == -1) {
+                        cout << "Nenhuma senha para atender." << endl;
+                    } else {
+                        enqueuePonteiro(aux->dado.SenhasAtendidasGuiche, senha);
+                        cout << "Senha atendida: "<< senha << endl;
+                    }
+                    IDencontrado = true;
+        		    break;
+        		}
+        		aux = aux->prox;
             }
-            default:
-                cout << "Digite uma opção válida" << endl;
-                break;
+            if (!encontrado) 
+            {
+                cout << "Nenhum guichê encontrado." << endl;
+            }
+            break;
+        }
+        case 4:
+            break;
+        
+        default:
+            cout << "Digite uma opção válida" << endl;
+            break;
         }
     } while(opcao != 0);
     
